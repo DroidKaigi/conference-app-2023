835beb5772ea70606fe228eec363c4c3a769f34c
RyuNen344 <s1100633@outlook.com>
2023-05-02 01:51:42 +0900

    chore(kmm): setup arch filter for iOS setup

--- build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KmpIosPlugin.kt
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KmpIosPlugin.kt
@@ -1,52 +1,72 @@
 package io.github.droidkaigi.confsched2023.primitive
 
+import io.github.droidkaigi.confsched2023.primitive.Arch.ALL
+import io.github.droidkaigi.confsched2023.primitive.Arch.ARM
+import io.github.droidkaigi.confsched2023.primitive.Arch.X86
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import java.util.Properties
 
 @Suppress("unused")
 class KmpIosPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             kotlin {
-                val isCD = System.getenv()["CD"]?.toBoolean() ?: false
-                val isCI = System.getenv()["CI"]?.toBoolean() ?: false
-                val iosMain = sourceSets.create("iosMain") {
-                    dependsOn(sourceSets.getByName("commonMain"))
-                }
-                // make build speed faster on CD
-                if (isCD) {
-                    iosArm64()
-                    sourceSets.getByName("iosArm64Main") {
-                        dependsOn(iosMain)
-                    }
-                // make build speed faster on CI
-                } else if (isCI) {
-                    iosX64()
-                    sourceSets.getByName("iosX64Main") {
-                        dependsOn(iosMain)
-                    }
-                } else {
-                    iosArm64()
-                    iosSimulatorArm64()
-                    iosX64()
-                    sourceSets.getByName("iosArm64Main") {
-                        dependsOn(iosMain)
-                    }
-                    sourceSets.getByName("iosSimulatorArm64Main") {
-                        dependsOn(iosMain)
-                    }
-                    sourceSets.getByName("iosX64Main") {
-                        dependsOn(iosMain)
+                val activeArch = Arch.findByArch(
+                    rootProject.layout.projectDirectory.file("local.properties").asFile.takeIf { it.exists() }
+                        ?.let {
+                            Properties().apply {
+                                load(it.reader(Charsets.UTF_8))
+                            }.getProperty("arch")
+                        } ?: System.getenv("arch")
+                )
+                when (activeArch) {
+                    ARM -> iosSimulatorArm64()
+                    X86 -> iosX64()
+                    ALL -> {
+                        iosArm64()
+                        iosX64()
+                        iosSimulatorArm64()
                     }
                 }
-                targets.withType<KotlinNativeTarget>().configureEach {
-                    binaries.all {
-                        binaryOptions["memoryModel"] = "experimental"
+                with(sourceSets) {
+                    create("iosMain") {
+                        dependsOn(getByName("commonMain"))
+                        maybeCreate("iosArm64Main").dependsOn(this)
+                        maybeCreate("iosX64Main").dependsOn(this)
+                        maybeCreate("iosSimulatorArm64Main").dependsOn(this)
                     }
+
+                    create("iosTest") {
+                        dependsOn(getByName("commonTest"))
+                        maybeCreate("iosArm64Test").dependsOn(this)
+                        maybeCreate("iosX64Test").dependsOn(this)
+                        maybeCreate("iosSimulatorArm64Test").dependsOn(this)
+                    }
+                }
+
+                targets.withType<KotlinNativeTarget> {
+                    // export kdoc to header file
+                    // https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+                    compilations["main"].kotlinOptions.freeCompilerArgs += "-Xexport-kdoc"
                 }
             }
+        }
+    }
+}
+
+enum class Arch(val arch: String?) {
+    ARM("arm64"),
+    X86("x86_64"),
+    ALL(null),
+    ;
+
+    companion object {
+        fun findByArch(arch: String?): Arch {
+            return values().firstOrNull { it.arch == arch } ?: ALL
         }
     }
 }
