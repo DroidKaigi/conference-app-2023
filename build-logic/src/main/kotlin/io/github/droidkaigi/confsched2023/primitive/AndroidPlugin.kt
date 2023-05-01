72eb6eca97c604d535aaa36abb75f8e22d349787
RyuNen344 <s1100633@outlook.com>
2023-05-02 01:07:10 +0900

    chore(kmm): decrease gradle agp version

--- build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/AndroidPlugin.kt
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/AndroidPlugin.kt
@@ -1,10 +1,7 @@
 package io.github.droidkaigi.confsched2023.primitive
 
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.kotlin.dsl.dependencies
 
 @Suppress("unused")
 class AndroidPlugin : Plugin<Project> {
