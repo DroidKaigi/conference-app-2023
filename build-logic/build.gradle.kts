4accca06fe6e06b2d697ba72c120921e4b716d47
RyuNen344 <s1100633@outlook.com>
2023-05-07 03:49:16 +0900

    chore: bump gradle to 8

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,6 +9,12 @@ repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
