a65ad74d467d4fe05d513c4cdcd44a8072f01430
takahirom <takam.dev@gmail.com>
2023-05-07 15:35:59 +0900

    Use colorScheme instead of colors

--- app-android/src/main/java/io/github/droidkaigi/confsched2023/MainActivity.kt
+++ app-android/src/main/java/io/github/droidkaigi/confsched2023/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     TimetableScreen()
                 }
