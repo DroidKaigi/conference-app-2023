6f215c6e826120ab3c94f80e4737301a268173b4
takahirom <takam.dev@gmail.com>
2023-05-14 13:06:39 +0900

    Introduce KMP Compose plugin

--- app-android/src/main/java/io/github/droidkaigi/confsched2023/MainActivity.kt
+++ app-android/src/main/java/io/github/droidkaigi/confsched2023/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TimetableScreen()
+                    ContributorsScreen()
                 }
             }
         }
---
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KmpComposePlugin.kt
@@ -0,0 +1,38 @@
+package io.github.droidkaigi.confsched2023.primitive
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.get
+
+@Suppress("unused")
+class KmpComposePlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("org.jetbrains.compose")
+            }
+            android {
+                buildFeatures.compose = true
+                composeOptions {
+                    kotlinCompilerExtensionVersion = libs.findLibrary("composeCompiler").get()
+                        .get().versionConstraint.requiredVersion
+                }
+            }
+            val compose = extensions.get("compose") as org.jetbrains.compose.ComposeExtension
+            kotlin {
+                with(sourceSets) {
+                    get("commonMain").apply {
+                        dependencies {
+                            implementation(compose.dependencies.runtime)
+                            implementation(compose.dependencies.foundation)
+                            implementation(compose.dependencies.material3)
+                            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                            implementation(compose.dependencies.components.resources)
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
