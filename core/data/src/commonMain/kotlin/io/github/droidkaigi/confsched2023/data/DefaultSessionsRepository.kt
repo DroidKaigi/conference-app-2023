ae05f60610bf41a5838e4e29c6c48f94633f5838
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2023-06-11 19:48:39 +0900

    [wip] Unify source with datastore
    sourceをdatastoreに統一

--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/data/DefaultSessionsRepository.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/data/DefaultSessionsRepository.kt
@@ -11,12 +11,11 @@ import io.github.droidkaigi.confsched2023.model.Timetable
 import io.github.droidkaigi.confsched2023.model.TimetableItemId
 import kotlinx.collections.immutable.PersistentSet
 import kotlinx.collections.immutable.persistentSetOf
-import kotlinx.collections.immutable.toPersistentSet
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
@@ -24,36 +23,11 @@ import kotlinx.coroutines.launch
 class DefaultSessionsRepository(
     private val sessionsApi: SessionsApi,
     private val dataStore: DataStore<Preferences>,
-    coroutineScope: CoroutineScope
+    private val coroutineScope: CoroutineScope
 ) : SessionsRepository {
 
     companion object {
         private val KEY_FAVORITE_SESSION_IDS = stringPreferencesKey("favorite_session_ids")
-    }
-
-    private val favoriteSessionIds: MutableStateFlow<PersistentSet<TimetableItemId>> =
-        MutableStateFlow(persistentSetOf())
-
-    init {
-        coroutineScope.launch {
-            dataStore.data
-                .catch { exception ->
-                    if (exception is IOException) {
-                        emit(emptyPreferences())
-                    } else {
-                        throw exception
-                    }
-                }
-                .map { preferences ->
-                    val favoriteIds = preferences[KEY_FAVORITE_SESSION_IDS] ?: ""
-                    persistentSetOf<TimetableItemId>().apply {
-                        favoriteIds.split(",").forEach { id ->
-                            add(TimetableItemId(id))
-                        }
-                    }
-                }
-                .collect { favoriteSessionIds.value = it }
-        }
     }
 
     private val timetableFlow: Flow<Timetable> = flow {
@@ -61,24 +35,42 @@ class DefaultSessionsRepository(
     }
 
     override fun getSessionsStream(): Flow<Timetable> {
-        return combine(timetableFlow, favoriteSessionIds) { timetable, favorites ->
+        return combine(timetableFlow, getFavoriteSessionStream()) { timetable, favorites ->
             timetable.copy(favorites = favorites)
         }
     }
 
+    private fun getFavoriteSessionStream(): Flow<PersistentSet<TimetableItemId>> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                val favoriteIds = preferences[KEY_FAVORITE_SESSION_IDS] ?: ""
+                persistentSetOf<TimetableItemId>().apply {
+                    favoriteIds.split(",").forEach { id ->
+                        add(TimetableItemId(id))
+                    }
+                }
+            }
+    }
+
     override suspend fun toggleFavorite(id: TimetableItemId) {
-        val updatedFavorites = favoriteSessionIds.value.toMutableSet()
+        val updatedFavorites = getFavoriteSessionStream().first()
 
         if (updatedFavorites.contains(id)) {
             updatedFavorites.remove(id)
         } else {
             updatedFavorites.add(id)
         }
-
-        dataStore.edit { preferences ->
-            preferences[KEY_FAVORITE_SESSION_IDS] = updatedFavorites.joinToString(",")
+        coroutineScope.launch {
+            dataStore.edit { preferences ->
+                preferences[KEY_FAVORITE_SESSION_IDS] = updatedFavorites.joinToString(",")
+            }
         }
-
-        favoriteSessionIds.value = updatedFavorites.toPersistentSet()
     }
 }
