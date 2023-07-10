d03032391e860ef9b811f50df3f67cfbe44c2639
ryoya ito <ry.itto.github@gmail.com>
2023-07-10 20:43:48 +0900

    move ViewBuilder to common module and rename to ViewProvider

--- app-ios/Modules/Sources/Model/ViewProvider.swift
+++ app-ios/Modules/Sources/Model/ViewProvider.swift
@@ -1,3 +1,3 @@
 import SwiftUI
 
-public typealias ViewBuilder<each Params, V: View> = (_ params: repeat each Params) -> V
+public typealias ViewProvider<each Params, V: View> = (_ params: repeat each Params) -> V
