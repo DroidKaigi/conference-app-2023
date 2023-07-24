dde2341ceb17846459c9292aead8aed3c8cb40c0
takahirom <takam.dev@gmail.com>
2023-07-24 11:07:49 +0900

    Fix format

--- core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/ImagePainter.kt
+++ core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/ImagePainter.kt
@@ -1,10 +1,7 @@
 package io.github.droidkaigi.confsched2023.ui
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import com.seiko.imageloader.rememberImagePainter
 
 @Composable
