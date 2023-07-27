3b923b6538379df77a25e2728c110e22d3830d8f
NUmeroAndDev <numero.dev.and@gmail.com>
2023-07-28 08:55:37 +0900

    Added Contributor model class

---
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/Contributor.kt
@@ -0,0 +1,8 @@
+package io.github.droidkaigi.confsched2023.model
+
+public data class Contributor(
+    val id: Int,
+    val username: String,
+    val profileUrl: String?,
+    val iconUrl: String,
+)
