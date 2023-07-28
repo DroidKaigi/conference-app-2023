0c032cf30e1567f7cc096cb68c783cb86ea0cbc2
NUmeroAndDev <numero.dev.and@gmail.com>
2023-07-28 09:36:47 +0900

    Implemented fake contributors data

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/Contributor.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/Contributor.kt
@@ -1,8 +1,24 @@
 package io.github.droidkaigi.confsched2023.model
 
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.toPersistentList
+
 public data class Contributor(
     val id: Int,
     val username: String,
     val profileUrl: String?,
     val iconUrl: String,
-)
+) {
+    public companion object
+}
+
+public fun Contributor.Companion.fakes(): PersistentList<Contributor> = (0..20)
+    .map {
+        Contributor(
+            id = it,
+            username = it.toString(),
+            profileUrl = "https://developer.android.com/",
+            iconUrl = "https://placehold.jp/150x150.png",
+        )
+    }
+    .toPersistentList()
