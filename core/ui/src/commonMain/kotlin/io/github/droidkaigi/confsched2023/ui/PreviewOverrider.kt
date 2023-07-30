3b61238ae8d1b803e13d4a31ab9e6579c6a662ef
takahirom <takam.dev@gmail.com>
2023-07-30 20:05:42 +0900

    Fix format

---
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/PreviewOverrider.kt
@@ -0,0 +1,12 @@
+package io.github.droidkaigi.confsched2023.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalInspectionMode
+
+@Composable
+fun Painter.overridePreviewWith(previewPainter: @Composable () -> Painter): Painter {
+    if (LocalInspectionMode.current) return previewPainter()
+    if (isTest()) return previewPainter()
+    return this
+}
--- core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/TestEnviroment.kt
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/TestEnviroment.kt
@@ -1,14 +1,3 @@
 package io.github.droidkaigi.confsched2023.ui
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.platform.LocalInspectionMode
-
-@Composable
-fun Painter.overridePreviewWith(previewPainter: @Composable () -> Painter): Painter {
-    if (LocalInspectionMode.current) return previewPainter()
-    if (isTest()) return previewPainter()
-    return this
-}
-
 expect fun isTest(): Boolean
