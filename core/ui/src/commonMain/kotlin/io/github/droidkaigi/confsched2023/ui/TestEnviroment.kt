9ef1c079d048b69e46f4eb922b789157285f97de
takahirom <takam.dev@gmail.com>
2023-07-30 18:29:54 +0900

    Fix format

--- core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/TestEnviroment.kt
+++ core/ui/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/TestEnviroment.kt
@@ -1,6 +1,5 @@
 package io.github.droidkaigi.confsched2023.ui
 
-import android.os.Build
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalInspectionMode
