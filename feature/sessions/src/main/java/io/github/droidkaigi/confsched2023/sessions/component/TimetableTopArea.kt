34d22d4bc79e26c4fcf49cf4012002c42c390952
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2023-08-02 11:42:06 +0900

    Change TopAppBar actions
    デザインを被せるように変更

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableTopArea.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableTopArea.kt
@@ -1,13 +1,7 @@
 package io.github.droidkaigi.confsched2023.sessions.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material.icons.filled.GridView
@@ -16,15 +10,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched2023.feature.sessions.R.drawable
 import io.github.droidkaigi.confsched2023.sessions.SessionsStrings.Search
 import io.github.droidkaigi.confsched2023.sessions.SessionsStrings.Timetable
@@ -36,7 +27,6 @@ const val TimetableBookmarkIconTestTag = "TimetableBookmarkIconTestTag"
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 fun TimetableTopArea(
-    state: TimetableScreenScrollState,
     onTimetableUiChangeClick: () -> Unit,
     onSearchClick: () -> Unit,
     onTopAreaBookmarkIconClick: () -> Unit,
@@ -84,27 +74,5 @@ fun TimetableTopArea(
                 containerColor = MaterialTheme.colorScheme.surfaceVariant,
             ),
         )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    state.onHeaderPositioned(coordinates.size.height.toFloat())
-                }
-                .height(168.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(Modifier.padding(start = 16.dp)) {
-                Text(text = "DroidKaigi\n2023", style = MaterialTheme.typography.displaySmall)
-                Spacer(modifier = Modifier.height(2.dp))
-                Text(
-                    text = "at Bellesalle Shibuya Garden",
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
-            Image(
-                painter = painterResource(id = drawable.img_keyvisual),
-                contentDescription = null
-            )
-        }
     }
 }
