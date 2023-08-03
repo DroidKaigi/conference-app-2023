a2394241c54ed587fb8fdb9c8d19e7da7a48c68b
swimmy-reo <reo.runningg@gmail.com>
2023-08-03 11:05:27 +0900

    Revert "Merge pull request #314 from DroidKaigi/swimmy-reo/apply-timetable-content-design"
    This reverts commit 5eaeefb5a0ca29df4351354bd0fab90cc406048a, reversing
    changes made to 5ddc0bcd9a12a4a2304b2f2d0ff738d960d3905f.

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
@@ -1,6 +1,5 @@
 package io.github.droidkaigi.confsched2023.sessions.component
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -31,9 +30,6 @@ fun TimetableItemDetailScreenTopAppBar(
             //  )
         },
         navigationIcon = {
-            Column {
-                
-            }
             IconButton(onClick = onNavigationIconClick) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
