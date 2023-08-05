2257ca859cb712d456c2811a107422c549fec944
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-08-06 00:29:11 +0900

    fix a format violation

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
@@ -54,7 +54,7 @@ fun TimetableItemDetailScreenTopAppBar(
                         MaterialTheme.typography.titleLarge,
                         MaterialTheme.typography.titleMedium,
                         MaterialTheme.typography.titleSmall,
-                    )
+                    ),
                 )
             }
         },
