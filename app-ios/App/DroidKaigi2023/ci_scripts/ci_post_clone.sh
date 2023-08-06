276e71f85342e50ef39b4f30dbdf91c444f20216
ryoya ito <ry.itto.github@gmail.com>
2023-08-07 03:20:06 +0900

    use parent dir

--- app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
+++ app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
@@ -5,4 +5,4 @@ brew install openjdk@17
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
 cd "$CI_WORKSPACE/app-ios"
-./gradlew :app-ios-shared:assembleSharedReleaseXCFramework
+../gradlew :app-ios-shared:assembleSharedReleaseXCFramework
