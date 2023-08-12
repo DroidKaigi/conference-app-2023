e9cf2ed6ff7fbac6b30b2e09f0bf90e2f6e4c31f
Tomoya Miwa <tomoyann@gmail.com>
2023-08-12 23:58:26 +0900

    Make pass distribution_name as a form string in destroy_distribution_by_name

--- src/actions/destroy_distribution_by_name/index.ts
+++ src/actions/destroy_distribution_by_name/index.ts
@@ -8,42 +8,22 @@ async function run(): Promise<void> {
     const platform: string = core.getInput('platform').toLowerCase()
     const appId: string = core.getInput('app-id')
     const distributionName: string = core.getInput('distribution-name')
-    const isTestMode: boolean = core.getInput('test') === 'true'
 
     if (!(platform === 'android' || platform === 'ios')) {
       throw new Error(`${platform} is not allowed`)
     }
+
+    const form = new FormData()
+
+    form.append('distribution_name', distributionName)
 
     const url = `https://deploygate.com/api/users/${appOwnerName}/platforms/${platform}/apps/${appId}/distributions/`
     const options = {
       headers: {
         Authorization: `token ${apiToken}`
       },
-      body: JSON.stringify({distribution_name: distributionName}),
+      body: form,
       method: 'DELETE'
-    }
-
-    core.debug(url)
-
-    if (isTestMode) {
-      core.setOutput('options', JSON.stringify(options))
-      core.setOutput('status', 200)
-      core.setOutput('response', JSON.stringify({dummy: true}))
-    } else {
-      const response = await fetch(
-        `https://deploygate.com/api/users/${appOwnerName}/platforms/${platform}/apps/${appId}/distributions/`,
-        {
-          headers: {
-            Authorization: `token ${apiToken}`
-          },
-          body: JSON.stringify({distribution_name: distributionName}),
-          method: 'DELETE'
-        }
-      )
-      const json = await response.json()
-
-      core.setOutput('status', response.status)
-      core.setOutput('response', JSON.stringify(json))
     }
   } catch (error) {
     if(error instanceof Error) {
