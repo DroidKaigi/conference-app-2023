5ab3389700020295fc1aacaeb16f2fa313de04a5
ryoya ito <ry.itto.github@gmail.com>
2023-08-13 19:08:03 +0900

    skip plugin validation

--- app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
+++ app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
@@ -6,3 +6,6 @@ export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
 cd "$CI_WORKSPACE"
 ./gradlew :app-ios-shared:assembleSharedReleaseXCFramework
+
+# Enalbe skip plugin validation
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
