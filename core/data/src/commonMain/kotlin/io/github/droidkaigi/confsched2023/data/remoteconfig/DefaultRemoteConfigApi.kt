b68b4201f238fffc8c6312eb39dbb8f2f4c042d6
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2023-08-14 21:12:42 +0900

    Added fetching interval adjustment
    フェッチする感覚の調整を追加

--- core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/data/remoteconfig/DefaultRemoteConfigApi.kt
+++ core/data/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/data/remoteconfig/DefaultRemoteConfigApi.kt
@@ -3,22 +3,21 @@ package io.github.droidkaigi.confsched2023.data.remoteconfig
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.remoteconfig.get
 import dev.gitlive.firebase.remoteconfig.remoteConfig
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
-import kotlinx.coroutines.launch
 
 class DefaultRemoteConfigApi : RemoteConfigApi {
 
     private val firebaseRemoteConfig = Firebase.remoteConfig
 
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            firebaseRemoteConfig.settings {
-                minimumFetchIntervalInSeconds = 10
-            }
-        }
-    }
+    /**
+     * If you want to change the interval time to fetch, please change it here
+     */
+//    init {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            firebaseRemoteConfig.settings {
+//                minimumFetchIntervalInSeconds = 12 * 3600L
+//            }
+//        }
+//    }
 
     override suspend fun getBoolean(key: String): Boolean {
         firebaseRemoteConfig.fetchAndActivate()
