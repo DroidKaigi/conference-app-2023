14a0f9b39042ae443aed829e0fb380a9d45db85f
Naoki Hidaka <naoki.hidaka@dena.com>
2023-08-14 23:08:11 +0900

    add staff module

---
+++ feature/staff/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("droidkaigi.convention.androidfeature")
+}
+
+android.namespace = "io.github.droidkaigi.confsched2023.feature.staff"
+
+dependencies {
+    implementation(projects.core.designsystem)
+    implementation(projects.core.ui)
+    implementation(projects.core.model)
+    testImplementation(projects.core.testing)
+
+    implementation(libs.androidxCoreKtx)
+    implementation(libs.composeUi)
+    implementation(libs.composeHiltNavigtation)
+    implementation(libs.composeMaterial)
+    implementation(libs.composeUiToolingPreview)
+    implementation(libs.androidxLifecycleLifecycleRuntimeKtx)
+    implementation(libs.androidxActivityActivityCompose)
+    implementation(libs.composeConstraintLayout)
+    implementation(libs.composeMaterialIcon)
+    androidTestImplementation(libs.composeUiTestJunit4)
+    debugImplementation(libs.composeUiTooling)
+    debugImplementation(libs.composeUiTestManifest)
+}
