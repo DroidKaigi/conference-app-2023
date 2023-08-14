f8c8affb17d074a4689ec5bd73a1b74d631c32ed
takahirom <takam.dev@gmail.com>
2023-08-14 12:39:56 +0900

    Use size for sponsors list

--- feature/sponsors/src/main/java/io/github/droidkaigi/confsched2023/sponsors/SponsorsScreen.kt
+++ feature/sponsors/src/main/java/io/github/droidkaigi/confsched2023/sponsors/SponsorsScreen.kt
@@ -78,9 +78,7 @@ private fun SponsorsScreen(
                     text = "Please implement SponsorsScreen!!!",
                     style = MaterialTheme.typography.titleLarge,
                 )
-                uiState.sponsors.forEach {
-                    Text(it.toString())
-                }
+                "uiState.sponsors.size:" + uiState.sponsors.size
             }
         },
     )
