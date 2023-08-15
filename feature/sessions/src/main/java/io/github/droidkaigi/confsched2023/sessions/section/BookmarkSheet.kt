192cb031fcdab5ed1dd05d823fe8608c88fc61ae
okuzawats <okuzawats@gmail.com>
2023-08-15 16:30:33 +0900

    use secondary container color as bookmark icon background

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/section/BookmarkSheet.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/section/BookmarkSheet.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -113,7 +114,7 @@ private fun EmptyView() {
             modifier = Modifier
                 .size(84.dp)
                 .background(
-                    color = Color(0xFFCEE9DB),
+                    color = MaterialTheme.colorScheme.secondaryContainer,
                     shape = RoundedCornerShape(24.dp),
                 ),
             contentAlignment = Alignment.Center,
