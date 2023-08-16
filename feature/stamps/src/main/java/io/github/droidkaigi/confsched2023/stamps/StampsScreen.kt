000faaa2fb9f70d1b2df86c282e732ae70d43788
5altNaCl <shouta02jp@gmail.com>
2023-08-16 13:08:53 +0900

    Implementation of stamps initial screen

--- feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsScreen.kt
+++ feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsScreen.kt
@@ -1,13 +1,16 @@
 package io.github.droidkaigi.confsched2023.stamps
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -16,14 +19,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import io.github.droidkaigi.confsched2023.feature.stamps.R
 import io.github.droidkaigi.confsched2023.ui.SnackbarMessageEffect
 import kotlinx.collections.immutable.ImmutableList
 
@@ -114,7 +120,64 @@ private fun StampsScreen(
                     style = MaterialTheme.typography.bodySmall,
                     modifier = Modifier.fillMaxWidth()
                 )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Stamps(stamps = uiState.stamps)
             }
         },
     )
+}
+
+@Composable
+private fun Stamps(
+    stamps: ImmutableList<String>,
+    modifier: Modifier = Modifier,
+) {
+    // TODO: Switching the display of stamps image according stamps variable
+    Column(modifier = modifier) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.img_stamp_a_off),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 21.dp)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.img_stamp_b_off),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 21.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.img_stamp_c_off),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 21.dp)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.img_stamp_d_off),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 21.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_stamp_e_off),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        )
+    }
 }
