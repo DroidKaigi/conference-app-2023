2f1b6eb4ef0710fbb2759e541384f36b1e98c5ec
Ichinose Yoshihiro <maruisansmai@gmail.com>
2023-08-16 15:13:35 +0900

    feat(floor-map): add string resource

--- feature/floor-map/src/main/java/io/github/droidkaigi/confsched2023/floormap/FloorMapStrings.kt
+++ feature/floor-map/src/main/java/io/github/droidkaigi/confsched2023/floormap/FloorMapStrings.kt
@@ -5,13 +5,24 @@ import io.github.droidkaigi.confsched2023.designsystem.strings.Strings
 import io.github.droidkaigi.confsched2023.designsystem.strings.StringsBindings
 
 sealed class FloorMapStrings : Strings<FloorMapStrings>(Bindings) {
+    data object Title : FloorMapStrings()
+    data object Basement : FloorMapStrings()
+    data object Ground : FloorMapStrings()
 
     private object Bindings : StringsBindings<FloorMapStrings>(
         Lang.Japanese to { item, _ ->
-            TODO()
+            when (item) {
+                Title -> "Floor Map"
+                Basement -> "1F"
+                Ground -> "B1F"
+            }
         },
         Lang.English to { item, bindings ->
-            TODO()
+            when (item) {
+                Title -> bindings.defaultBinding(item, bindings)
+                Basement -> bindings.defaultBinding(item, bindings)
+                Ground -> bindings.defaultBinding(item, bindings)
+            }
         },
         default = Lang.Japanese,
     )
