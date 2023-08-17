d037591272930e39b83c157b71ee62c15acf7508
sanao1006 <naosak1006@gmail.com>
2023-08-17 23:41:44 +0900

    Simplify ReadMoreOutlinedButton Click State Change

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailContent.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailContent.kt
@@ -92,7 +92,7 @@ private fun DescriptionSection(
         )
         if (!isExpanded) {
             ReadMoreOutlinedButton(
-                onClick = { isExpanded = !isExpanded },
+                onClick = { isExpanded = true },
                 modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
             )
         }
