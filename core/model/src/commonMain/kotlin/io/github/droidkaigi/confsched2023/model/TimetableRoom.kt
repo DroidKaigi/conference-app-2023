934cc9175f30daa81a11556062b4d97d2151edb7
hiraike32 <takuya_hiraike@dwango.co.jp>
2023-08-18 16:46:17 +0900

    Show floor infomation with room on session's detail page

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/TimetableRoom.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/TimetableRoom.kt
@@ -9,3 +9,16 @@ public data class TimetableRoom(
 
 val TimetableRoom.type: RoomIndex
     get() = RoomIndex.entries.getOrNull(sortIndex) ?: RoomIndex.Room1
+
+val TimetableRoom.nameAndFloor: String
+    get() {
+        val basementFloorString = MultiLangText(jaTitle = "地下1階", enTitle = "B1F")
+        val floor = when (type) {
+            RoomIndex.Room1 -> basementFloorString.currentLangTitle
+            RoomIndex.Room2 -> basementFloorString.currentLangTitle
+            RoomIndex.Room3 -> basementFloorString.currentLangTitle
+            RoomIndex.Room4 -> "1F"
+            RoomIndex.Room5 -> "1F"
+        }
+        return "${name.currentLangTitle} ($floor)"
+    }
