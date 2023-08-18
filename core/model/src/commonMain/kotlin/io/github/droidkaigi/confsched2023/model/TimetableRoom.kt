fa7c722247dd4b26b81b9b8d8be61a43a1f03f9c
mangano-ito <47137677+mangano-ito@users.noreply.github.com>
2023-08-18 17:07:51 +0900

    use name for the room type mapping
    - sort index may be not suitable for this use case

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/TimetableRoom.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/TimetableRoom.kt
@@ -1,5 +1,11 @@
 package io.github.droidkaigi.confsched2023.model
 
+import io.github.droidkaigi.confsched2023.model.RoomIndex.Room1
+import io.github.droidkaigi.confsched2023.model.RoomIndex.Room2
+import io.github.droidkaigi.confsched2023.model.RoomIndex.Room3
+import io.github.droidkaigi.confsched2023.model.RoomIndex.Room4
+import io.github.droidkaigi.confsched2023.model.RoomIndex.Room5
+
 public data class TimetableRoom(
     val id: Int,
     val name: MultiLangText,
@@ -8,4 +14,11 @@ public data class TimetableRoom(
 )
 
 val TimetableRoom.type: RoomIndex
-    get() = RoomIndex.entries.getOrNull(sortIndex) ?: RoomIndex.Room1
+    get() = when (name.enTitle.lowercase()) {
+        "arctic fox" -> Room2
+        "bumblebee" -> Room3
+        "chipmunk" -> Room1
+        "dolphin" -> Room4
+        "electric eel" -> Room5
+        else -> Room1
+    }
