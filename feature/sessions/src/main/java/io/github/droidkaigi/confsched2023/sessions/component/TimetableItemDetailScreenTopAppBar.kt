aec733bdcb3d25b65bea8f3cb39bcdf9cecc94f3
kosenda <ksnd@KSND-MacBook-Pro.local>
2023-08-18 11:10:38 +0900

    fix: change TopBarHeight value and paragraph function

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
@@ -14,15 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFontFamilyResolver
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.MultiParagraph
-import androidx.compose.ui.text.MultiParagraphIntrinsics
+import androidx.compose.ui.text.Paragraph
+import androidx.compose.ui.text.ParagraphIntrinsics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Constraints
@@ -30,8 +29,8 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import io.github.droidkaigi.confsched2023.model.MultiLangText
 
-const val LARGE_TOP_APP_BAR_HEIGHT_DP = 72f
-const val RATE_TO_REDUCE_FONT_SIZE = 0.95f
+const val LARGE_TOP_APP_BAR_HEIGHT_DP = 124f // maxTopAppBarHeight(152.dp) - bottomPadding(28.dp)
+const val RATE_TO_REDUCE_FONT_SIZE = 0.92f
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -94,14 +93,14 @@ private fun ResizeableText(
     overflow: TextOverflow,
     minFontSize: TextUnit = MaterialTheme.typography.labelSmall.fontSize,
 ) {
-    var styleIndex by remember(text) { mutableStateOf(0) }
+    var styleIndex by remember(text) { mutableIntStateOf(0) }
     val density = LocalDensity.current
     var fontSize by remember(text) { mutableFloatStateOf(styles.first().fontSize.value) }
 
-    val calculateMultiParagraph = @Composable {
-        MultiParagraph(
-            intrinsics = MultiParagraphIntrinsics(
-                annotatedString = AnnotatedString(text = text),
+    val calculateParagraph = @Composable {
+        Paragraph(
+            paragraphIntrinsics = ParagraphIntrinsics(
+                text = text,
                 style = styles[styleIndex].copy(fontSize = fontSize.sp),
                 placeholders = emptyList(),
                 density = density,
@@ -113,10 +112,11 @@ private fun ResizeableText(
         )
     }
 
-    var intrinsics = calculateMultiParagraph()
+    // calculate before displaying to avoid broken character.
+    var intrinsics = calculateParagraph()
     while (intrinsics.height > LARGE_TOP_APP_BAR_HEIGHT_DP && fontSize > minFontSize.value) {
         if (styleIndex == styles.lastIndex) {
-            // change font size
+            // if the size does not fit in any style, reduce the font size.
             fontSize *= RATE_TO_REDUCE_FONT_SIZE
         } else {
             // switch to the next text style
@@ -124,8 +124,9 @@ private fun ResizeableText(
             styleIndex = nextIndex
             fontSize = styles[nextIndex].fontSize.value
         }
-        intrinsics = calculateMultiParagraph()
+        intrinsics = calculateParagraph()
     }
+
     Text(
         text = text,
         fontSize = fontSize.sp,
