cb151c92c8bf81d166dbdcc53043f0c8deb6c268
fumiyasac <just1factory@gmail.com>
2023-08-19 23:18:27 +0900

    add Dynamic Tab Animation in TimetableDayHeader.swift

--- app-ios/Modules/Sources/Timetable/TimetableDayHeader.swift
+++ app-ios/Modules/Sources/Timetable/TimetableDayHeader.swift
@@ -9,39 +9,62 @@ struct TimetableDayHeader: View {
     let onSelect: (DroidKaigi2023Day) -> Void
 
     var body: some View {
-        HStack(spacing: 8) {
-            ForEach(
-                [DroidKaigi2023Day].fromKotlinArray(DroidKaigi2023Day.values()),
-                id: \.ordinal
-            ) { (day: DroidKaigi2023Day) in
-                Button {
-                    onSelect(day)
-                } label: {
-                    VStack(spacing: 0) {
-                        Text(day.name)
-                            .font(Font.system(size: 12, weight: .semibold))
-                        Text("\(day.dayOfMonth)")
-                            .font(Font.system(size: 24, weight: .semibold))
-                            .frame(height: 32)
+        ZStack {
+            AssetColors.Surface.surface.swiftUIColor
+            HStack(spacing: 8) {
+                ForEach(
+                    [DroidKaigi2023Day].fromKotlinArray(DroidKaigi2023Day.values()),
+                    id: \.ordinal
+                ) { (day: DroidKaigi2023Day) in
+                    Button {
+                        onSelect(day)
+                    } label: {
+                        VStack(spacing: 0) {
+                            Text(day.name)
+                                .font(Font.system(size: 12, weight: .semibold))
+                            Text("\(day.dayOfMonth)")
+                                .font(Font.system(size: 24, weight: .semibold))
+                                .frame(height: 32)
+                        }
+                        .padding(4)
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(
+                            selectedDay == day
+                            ? AssetColors.Primary.onPrimary.swiftUIColor
+                            : AssetColors.Surface.onSurfaceVariant.swiftUIColor)
                     }
-                    .padding(4)
-                    .frame(maxWidth: .infinity)
-                    .foregroundStyle(
-                        selectedDay == day
-                        ? AssetColors.Primary.onPrimary.swiftUIColor
-                        : AssetColors.Surface.onSurfaceVariant.swiftUIColor)
-                    .background(
-                        selectedDay == day
-                        ? AssetColors.Primary.primary.swiftUIColor
-                        : Color.clear
-                    )
-                    .clipShape(Capsule())
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+            .background(alignment: .center) {
+                GeometryReader { geometry in
+                    Capsule()
+                        .fill(AssetColors.Primary.primary.swiftUIColor)
+                        .frame(width: calcButtonWidth(width: geometry.size.width), height: 56)
+                        .offset(x: calcDynamicTabHorizontalOffset(width: geometry.size.width), y: 10)
+                        .animation(.easeInOut(duration: 0.16), value: selectedDay)
                 }
             }
         }
-        .padding(.horizontal, 16)
-        .padding(.vertical, 12)
-        .background(AssetColors.Surface.surface.swiftUIColor)
+    }
+
+    private func calcDynamicTabHorizontalOffset(width: CGFloat) -> CGFloat {
+        let buttonAreaWidth = calcButtonWidth(width: width)
+        switch selectedDay {
+        case .day1:
+            return 16.0
+        case .day2:
+            return 24.0 + buttonAreaWidth
+        case .day3:
+            return 32.0 + buttonAreaWidth * 2
+        default:
+            return 24.0 + buttonAreaWidth
+        }
+    }
+
+    private func calcButtonWidth(width: CGFloat) -> CGFloat {
+        (width - 48.0) / 3.0
     }
 }
 
