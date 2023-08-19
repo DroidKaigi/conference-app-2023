d7bc7b310fb7da91daf784f489b98176740a4c6c
ryoya ito <ry.itto.github@gmail.com>
2023-08-19 18:45:39 +0900

    fix timetable cache

--- app-ios/Modules/Sources/Timetable/TimetableViewModel.swift
+++ app-ios/Modules/Sources/Timetable/TimetableViewModel.swift
@@ -13,31 +13,17 @@ struct TimetableState: ViewModelState {
 final class TimetableViewModel: ObservableObject {
     @Dependency(\.sessionsData) var sessionsData
     @Published private(set) var state: TimetableState = .init()
-    private var cachedTimeGroupTimetableItems: [TimetableTimeGroupItems]?
+    private var cachedTimetable: Timetable? {
+        didSet {
+            applySelectedDayToState()
+        }
+    }
 
     func load() async {
         state.timeGroupTimetableItems = .loading
         do {
             let timetable = try await sessionsData.timetable()
-            let timetableTimeGroupItems = timetable.dayTimetable(droidKaigi2023Day: state.selectedDay).timetableItems
-                .map {
-                    TimetableTimeGroupItems.Duration(startsAt: $0.startsAt, endsAt: $0.endsAt)
-                }
-                .map { duration in
-                    let items = timetable.contents
-                        .filter { itemWithFavorite in
-                            itemWithFavorite.timetableItem.startsAt == duration.startsAt && itemWithFavorite.timetableItem.endsAt == duration.endsAt
-                        }
-                        .sorted {
-                            $0.timetableItem.room.sort < $1.timetableItem.room.sort
-                        }
-                    return TimetableTimeGroupItems(
-                        duration: duration,
-                        items: items
-                    )
-                }
-            cachedTimeGroupTimetableItems = timetableTimeGroupItems
-            applySelectedDayToState()
+            cachedTimetable = timetable
         } catch let error {
             state.timeGroupTimetableItems = .failed(error)
         }
@@ -49,13 +35,29 @@ final class TimetableViewModel: ObservableObject {
     }
 
     private func applySelectedDayToState() {
-        guard let cachedTimeGroupTimetableItems = cachedTimeGroupTimetableItems else {
+        guard let cachedTimetable = cachedTimetable else {
             return
         }
-        state.timeGroupTimetableItems = .loaded(
-            cachedTimeGroupTimetableItems.filter {
-                $0.items.first?.timetableItem.day == state.selectedDay
+        let timetableTimeGroupItems = cachedTimetable.dayTimetable(droidKaigi2023Day: state.selectedDay)
+            .timetableItems
+            .map {
+                TimetableTimeGroupItems.Duration(startsAt: $0.startsAt, endsAt: $0.endsAt)
             }
+            .map { duration in
+                let items = cachedTimetable.contents
+                    .filter { itemWithFavorite in
+                        itemWithFavorite.timetableItem.startsAt == duration.startsAt && itemWithFavorite.timetableItem.endsAt == duration.endsAt
+                    }
+                    .sorted {
+                        $0.timetableItem.room.sort < $1.timetableItem.room.sort
+                    }
+                return TimetableTimeGroupItems(
+                    duration: duration,
+                    items: items
+                )
+            }
+        state.timeGroupTimetableItems = .loaded(
+            timetableTimeGroupItems
         )
     }
 }
