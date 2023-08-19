afb7b7b1e71e062ac84f8a8259fe812146db497f
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-08-19 18:47:00 +0900

    update fake data

--- core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/Timetable.kt
+++ core/model/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/model/Timetable.kt
@@ -4,6 +4,7 @@ import io.github.droidkaigi.confsched2023.model.RoomType.RoomA
 import io.github.droidkaigi.confsched2023.model.RoomType.RoomB
 import io.github.droidkaigi.confsched2023.model.RoomType.RoomC
 import io.github.droidkaigi.confsched2023.model.RoomType.RoomD
+import io.github.droidkaigi.confsched2023.model.RoomType.RoomE
 import io.github.droidkaigi.confsched2023.model.TimetableItem.Session
 import kotlinx.collections.immutable.PersistentSet
 import kotlinx.collections.immutable.persistentListOf
@@ -102,10 +103,11 @@ public fun Timetable?.orEmptyContents(): Timetable = this ?: Timetable()
 
 public fun Timetable.Companion.fake(): Timetable {
     var rooms = listOf(
-        TimetableRoom(1, MultiLangText("App Bar", "App Bar"), 0, 0, RoomA),
-        TimetableRoom(2, MultiLangText("Backdrop", "Backdrop"), 1, 1, RoomB),
-        TimetableRoom(3, MultiLangText("Cards", "Cards"), 2, 2, RoomC),
-        TimetableRoom(4, MultiLangText("Dialogs", "Dialogs"), 3, 3, RoomD),
+        TimetableRoom(1, MultiLangText("Arctic Fox", "Arctic Fox"), 0, 0, RoomA),
+        TimetableRoom(2, MultiLangText("Bumblebee", "Bumblebee"), 1, 1, RoomB),
+        TimetableRoom(3, MultiLangText("Chipmunk", "Chipmunk"), 2, 2, RoomC),
+        TimetableRoom(4, MultiLangText("Dolphin", "Dolphin"), 3, 3, RoomD),
+        TimetableRoom(5, MultiLangText("Electric Eel", "Electric Eel"), 4, 4, RoomE),
     )
     (0..10).forEach { _ ->
         rooms += rooms
