b5e253b13bdd39dd6e0d38907841111ff1a27f1b
yosuke.fujii <yosuke.fujii@photoruction.com>
2023-08-20 23:22:04 +0900

    make HoursItem

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableGridHours.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableGridHours.kt
@@ -1,6 +1,23 @@
 package io.github.droidkaigi.confsched2023.sessions.component
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun HoursItem(
+    hour: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = hour,
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleMedium
+    )
+}
 
 @Composable
 fun TimetableGridHours() {}
