494386f5ca83017bcceb49856cb62d9251dcb2d2
Masatoshi Kubode <swi.masatoshi@gmail.com>
2023-08-20 22:37:00 +0900

    Use `@OptionalExpectation` instead

--- core/common/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/Annotations.kt
+++ core/common/src/commonMain/kotlin/io/github/droidkaigi/confsched2023/ui/Annotations.kt
@@ -1,4 +1,9 @@
 package io.github.droidkaigi.confsched2023.ui
 
+@OptIn(ExperimentalMultiplatform::class)
+@OptionalExpectation
 expect annotation class KmpHiltViewModel()
+
+@OptIn(ExperimentalMultiplatform::class)
+@OptionalExpectation
 expect annotation class KmpInject()
