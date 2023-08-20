0b8995106c685b70776dfca8c2b25bcb8329ece8
coffmark <come1234560@gmail.com>
2023-08-20 18:31:37 +0900

    Switch between 1F and B1F

--- app-ios/Modules/Sources/FloorMap/FloorMapView.swift
+++ app-ios/Modules/Sources/FloorMap/FloorMapView.swift
@@ -3,33 +3,56 @@ import SwiftUI
 import Theme
 
 public struct FloorMapView: View {
+    enum Floor {
+        case first
+        case basementFirst
+    }
+
+    @State private var floor: Floor = .first
+
     public init() {}
 
     public var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack {
-                    VStack(alignment: .leading) {
-                        Text("1F")
-                            .font(Font.system(size: 24, weight: .medium))
-                        Assets.Images.floor1F.swiftUIImage
+            ZStack {
+                ScrollView {
+                    switch floor {
+                    case .first: firstFloorMapView()
+                    case .basementFirst: basementFirstFloorMapView()
                     }
-                    .padding(.vertical, 24)
-
-                    VStack(alignment: .leading) {
-                        Text("B1F")
-                            .font(Font.system(size: 24, weight: .medium))
-                        Assets.Images.floorB1F.swiftUIImage
-                    }
-                    .padding(.vertical, 24)
                 }
                 .foregroundStyle(AssetColors.Primary.primary.swiftUIColor)
+                VStack(spacing: 0) {
+                    Spacer()
+                    FloorMapFooterSegmentedButton(floor: $floor)
+                        .padding(.bottom, 24)
+                }
             }
             .navigationTitle("Floor Map")
         }
     }
+
+    /// first floor map
+    private func firstFloorMapView() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("1F")
+                .font(Font.system(size: 24, weight: .medium))
+            Assets.Images.floor1F.swiftUIImage
+        }
+        .padding(.vertical, 24)
+    }
+
+    /// basement first floor map
+    private func basementFirstFloorMapView() -> some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("B1F")
+                .font(Font.system(size: 24, weight: .medium))
+            Assets.Images.floorB1F.swiftUIImage
+        }
+        .padding(.vertical, 24)
+    }
 }
 
- #Preview {
-     FloorMapView()
- }
+#Preview {
+    FloorMapView()
+}
