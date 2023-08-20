3e39b213768fa78b497d654251dddea404fb633d
matsumo0922 <caios.system@gmail.com>
2023-08-20 12:29:41 +0900

    define AppModule and provide AppVersion

---
+++ app-android/src/main/java/io/github/droidkaigi/confsched2023/AppModule.kt
@@ -0,0 +1,24 @@
+package io.github.droidkaigi.confsched2023
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.droidkaigi.confsched2023.model.AppVersion
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAppVersion(): AppVersion {
+        return AppVersion(
+            packageName = BuildConfig.APPLICATION_ID,
+            versionName = BuildConfig.VERSION_NAME,
+            versionCode = BuildConfig.VERSION_CODE,
+            isDebug = BuildConfig.DEBUG,
+        )
+    }
+}
