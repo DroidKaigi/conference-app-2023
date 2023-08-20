632f39970457b61709ab07964a79694c28a8531d
KaitoMuraoka <kaitoboshi@icloud.com>
2023-08-20 21:36:48 +0900

    Revert "install swift-format"
    This reverts commit 7ae4c51ad241cd502d05d3e668243ad869202823.

--- app-ios/Modules/Package.swift
+++ app-ios/Modules/Package.swift
@@ -22,7 +22,6 @@ var package = Package(
         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
         .package(url: "https://github.com/realm/SwiftLint", from: "0.52.4"),
         .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", from: "6.6.2"),
-        .package(url: "https://github.com/apple/swift-format", from: "0.50700.1"),
     ],
     targets: [
         .target(
