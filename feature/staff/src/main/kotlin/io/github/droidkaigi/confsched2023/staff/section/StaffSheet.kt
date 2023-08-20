4f2aaccd490c8155d1e7f6d451ed97c466024040
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2023-08-20 12:43:29 +0900

    changed the Loading component used in the StaffSheet to use the LoadingText component from the designsystem/component.

--- feature/staff/src/main/kotlin/io/github/droidkaigi/confsched2023/staff/section/StaffSheet.kt
+++ feature/staff/src/main/kotlin/io/github/droidkaigi/confsched2023/staff/section/StaffSheet.kt
@@ -1,19 +1,10 @@
 package io.github.droidkaigi.confsched2023.staff.section
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
+import io.github.droidkaigi.confsched2023.designsystem.component.LoadingText
 import io.github.droidkaigi.confsched2023.model.Staff
 import io.github.droidkaigi.confsched2023.staff.section.StaffSheetUiState.Loading
 import io.github.droidkaigi.confsched2023.staff.section.StaffSheetUiState.StaffList
@@ -34,36 +25,11 @@ internal fun StaffSheet(
 ) {
     Box(modifier = modifier.fillMaxSize()) {
         when (uiState) {
-            Loading -> LoadingContent(modifier = Modifier.fillMaxSize())
+            Loading -> LoadingText(modifier = Modifier.fillMaxSize())
             is StaffList -> StaffList(
                 staffs = uiState.staffs,
                 onStaffClick = onStaffClick,
             )
         }
-    }
-}
-
-// FIXME: It might be a good idea to move this Composable function to a common module somewhere. The same applies to TimetableLoadingContent as well.
-@Composable
-private fun LoadingContent(
-    modifier: Modifier = Modifier,
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(
-            space = 16.dp,
-            alignment = Alignment.CenterVertically,
-        ),
-    ) {
-        CircularProgressIndicator()
-
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "Loading...",
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.bodyMedium,
-            overflow = TextOverflow.Ellipsis,
-        )
     }
 }
