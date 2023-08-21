4e028bbd5426a4a1ea7ee35e984a386310ddda89
todayama_r <13657682+Corvus400@users.noreply.github.com>
2023-08-22 07:21:34 +0900

    :wrench: Corresponds to SwallowedException.

--- core/designsystem/src/iosMain/kotlin/io.github.droidkaigi.confsched2023.designsystem/theme/FontResource.kt
+++ core/designsystem/src/iosMain/kotlin/io.github.droidkaigi.confsched2023.designsystem/theme/FontResource.kt
@@ -28,7 +28,7 @@ actual fun fontFamilyResource(fontResource: FontResource): MutableState<FontFami
                 fontMap[fontResource.resName] = FontFamily(font)
                 fontMap[fontResource.resName]
             } catch (e: Exception) {
-                throw NoSuchElementException("Please check if the font file specification is correct.")
+                throw NoSuchElementException(e.message)
             }
         }
     }
