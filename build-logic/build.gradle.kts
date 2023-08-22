0879708cf3f4e3cb4e95ae999241e63ecba8ac7c
takahirom <takam.dev@gmail.com>
2023-08-22 11:42:51 +0900

    Introduce kmp native coroutines

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -76,6 +76,10 @@ gradlePlugin {
             id = "droidkaigi.primitive.kmp.compose"
             implementationClass = "io.github.droidkaigi.confsched2023.primitive.KmpComposePlugin"
         }
+        register("kotlinMppNativeCoroutines") {
+            id = "droidkaigi.primitive.kmp.native.coroutines"
+            implementationClass = "io.github.droidkaigi.confsched2023.primitive.KmpNativeCoroutinesPlugin"
+        }
         register("kotlinMppKtorfit") {
             id = "droidkaigi.primitive.kmp.ktorfit"
             implementationClass = "io.github.droidkaigi.confsched2023.primitive.KmpKtorfitPlugin"
@@ -100,6 +104,7 @@ gradlePlugin {
             id = "droidkaigi.primitive.detekt"
             implementationClass = "io.github.droidkaigi.confsched2023.primitive.DetektPlugin"
         }
+
 
         // Conventions
         register("androidFeature") {
