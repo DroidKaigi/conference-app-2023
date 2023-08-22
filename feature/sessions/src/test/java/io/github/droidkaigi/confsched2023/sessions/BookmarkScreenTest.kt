5db331500258b14d7ce56ff99fc0a3ae9af44e81
Kazunori Jo <519604+itochan@users.noreply.github.com>
2023-08-22 18:10:32 +0900

    Add BookmarkScreenTest

---
+++ feature/sessions/src/test/java/io/github/droidkaigi/confsched2023/sessions/BookmarkScreenTest.kt
@@ -0,0 +1,41 @@
+package io.github.droidkaigi.confsched2023.sessions
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.github.droidkaigi.confsched2023.testing.HiltTestActivity
+import io.github.droidkaigi.confsched2023.testing.RobotTestRule
+import io.github.droidkaigi.confsched2023.testing.category.ScreenshotTests
+import io.github.droidkaigi.confsched2023.testing.robot.BookmarkScreenRobot
+import org.junit.Rule
+import org.junit.Test
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+import javax.inject.Inject
+
+@RunWith(AndroidJUnit4::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@HiltAndroidTest
+@Config(
+    qualifiers = RobolectricDeviceQualifiers.NexusOne,
+)
+class BookmarkScreenTest {
+
+    @get:Rule
+    @BindValue val robotTestRule: RobotTestRule = RobotTestRule<HiltTestActivity>(this)
+
+    @Inject
+    lateinit var bookmarkScreenRobot: BookmarkScreenRobot
+
+    @Test
+    @Category(ScreenshotTests::class)
+    fun checkLaunchShot() {
+        bookmarkScreenRobot {
+            setupBookmarkScreenContent()
+            checkScreenCapture()
+        }
+    }
+}
