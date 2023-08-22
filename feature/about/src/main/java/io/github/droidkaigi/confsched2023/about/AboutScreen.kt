a35973db08cafcded218e144636bac1a059b56ea
matsumo0922 <caios.system@gmail.com>
2023-08-22 21:53:30 +0900

    use remember fore more performance in AboutScreen#versionName

--- feature/about/src/main/java/io/github/droidkaigi/confsched2023/about/AboutScreen.kt
+++ feature/about/src/main/java/io/github/droidkaigi/confsched2023/about/AboutScreen.kt
@@ -62,12 +62,13 @@ const val AboutScreenTestTag = "AboutScreen"
 @Composable
 fun AboutScreen(
     onAboutItemClick: (AboutItem) -> Unit,
-    versionName: String? = versionName(LocalContext.current),
     viewModel: AboutScreenViewModel = hiltViewModel<AboutScreenViewModel>(),
     onLinkClick: (url: String) -> Unit,
 ) {
     // val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
     val snackbarHostState = remember { SnackbarHostState() }
+    val versionName = remember { versionName(context) }
 
     SnackbarMessageEffect(
         snackbarHostState = snackbarHostState,
@@ -91,7 +92,7 @@ private fun AboutScreen(
     snackbarHostState: SnackbarHostState,
     onAboutItemClick: (AboutItem) -> Unit,
     versionName: String?,
-    onLinkClick: (url: String) -> Unit,
+    onLinkClick: (url: String) -> Unit
 ) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
     Scaffold(
