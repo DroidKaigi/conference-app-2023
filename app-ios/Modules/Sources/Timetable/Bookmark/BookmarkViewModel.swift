db58389e4af8c9015c11254a1f6e10e1675cdb55
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2023-08-23 13:51:31 +0900

    Fix iOS code

--- app-ios/Modules/Sources/Timetable/Bookmark/BookmarkViewModel.swift
+++ app-ios/Modules/Sources/Timetable/Bookmark/BookmarkViewModel.swift
@@ -52,7 +52,7 @@ final class BookmarkViewModel: ObservableObject {
                         itemWithFavorite.timetableItem.startsAt == duration.startsAt && itemWithFavorite.timetableItem.endsAt == duration.endsAt
                     }
                     .sorted {
-                        $0.timetableItem.room.sort < $1.timetableItem.room.sort
+                        $0.timetableItem.room.name.currentLangTitie < $1.timetableItem.room.name.currentLangTitle
                     }
                 return TimetableTimeGroupItems(
                     duration: duration,
--- app-ios/Modules/Sources/Timetable/TimetableViewModel.swift
+++ app-ios/Modules/Sources/Timetable/TimetableViewModel.swift
@@ -49,7 +49,7 @@ final class TimetableViewModel: ObservableObject {
                         itemWithFavorite.timetableItem.startsAt == duration.startsAt && itemWithFavorite.timetableItem.endsAt == duration.endsAt
                     }
                     .sorted {
-                        $0.timetableItem.room.sort < $1.timetableItem.room.sort
+                        $0.timetableItem.room.name.currentLangTitie < $1.timetableItem.room.name.currentLangTitle
                     }
                 return TimetableTimeGroupItems(
                     duration: duration,
