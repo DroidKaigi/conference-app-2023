72b51f7cc92469e526fd8b4664081e0ce98ace8f
yosuke.fujii <yosuke.fujii@photoruction.com>
2023-08-24 00:54:24 +0900

    add itemProvider for TimetableGridHours

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableGridHours.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableGridHours.kt
@@ -1,5 +1,8 @@
 package io.github.droidkaigi.confsched2023.sessions.component
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.lazy.layout.LazyLayout
+import androidx.compose.foundation.lazy.layout.LazyLayoutItemProvider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,10 +22,42 @@ fun HoursItem(
     )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TimetableGridHours(
     modifier: Modifier = Modifier,
     content: @Composable (String) -> Unit,
 ) {
+    val itemProvider = itemProvider({ hoursList.size }) { index ->
+        content(hoursList[index])
+    }
 
 }
+
+@OptIn(ExperimentalFoundationApi::class)
+private fun itemProvider(
+    itemCount: () -> Int,
+    itemContent: @Composable (Int) -> Unit
+): LazyLayoutItemProvider {
+    return object : LazyLayoutItemProvider {
+        @Composable
+        override fun Item(index: Int, key: Any) {
+            itemContent(index)
+        }
+
+        override val itemCount: Int get() = itemCount()
+    }
+}
+
+private val hoursList = listOf(
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+)
