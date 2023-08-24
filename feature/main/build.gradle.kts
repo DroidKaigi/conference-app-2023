c73d5aaf4ca422ae1a08203ed562f0c168786c59
fumiya-kume <fumiya.kume@hotmail.com>
2023-08-25 00:55:00 +0900

    Remove core module dependency from the feature/main

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 android.namespace = "io.github.droidkaigi.confsched2023.feature.main"
 
 dependencies {
-    implementation(projects.core.designsystem)
-    implementation(projects.core.ui)
-    implementation(projects.core.model)
     testImplementation(projects.core.testing)
 
     implementation(libs.composeHiltNavigtation)
