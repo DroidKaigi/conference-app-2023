7f285c5d6812da0727e96352f987aa580790d4ce
Takao Chiba <takao.chiba@gmail.com>
2023-08-24 17:30:05 +0700

    Remove empty constructor and redundant space of empty line

--- core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/TimeZoneTestRule.kt
+++ core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/TimeZoneTestRule.kt
@@ -4,14 +4,14 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import java.util.TimeZone
 
-class TimeZoneTestRule() : TestWatcher() {
+class TimeZoneTestRule : TestWatcher() {
     private lateinit var evacuatedTimeZone: TimeZone
 
     override fun starting(description: Description) {
         evacuatedTimeZone = TimeZone.getDefault()
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"))
     }
-    
+
     override fun finished(description: Description) {
         TimeZone.setDefault(evacuatedTimeZone)
     }
