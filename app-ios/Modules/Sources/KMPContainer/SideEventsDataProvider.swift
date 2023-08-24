aa133ce70d82232ee0a6aaee53e19db2ad4655ae
coffmark <come1234560@gmail.com>
2023-08-24 10:04:05 +0900

    Add SideEventsDataProvider

---
+++ app-ios/Modules/Sources/KMPContainer/SideEventsDataProvider.swift
@@ -0,0 +1,20 @@
+import Dependencies
+import shared
+
+public struct SideEventsDataProvider {
+    public let sideEvents: () async throws -> [SideEvent]
+}
+
+extension SideEventsDataProvider: DependencyKey {
+    public static var liveValue: SideEventsDataProvider = SideEventsDataProvider(
+        sideEvents: {
+            return SideEventKt.SideEvents
+    })
+}
+
+public extension DependencyValues {
+    var sideEventsData: SideEventsDataProvider {
+        get { self[SideEventsDataProvider.self] }
+        set { self[SideEventsDataProvider.self] = newValue }
+    }
+}
