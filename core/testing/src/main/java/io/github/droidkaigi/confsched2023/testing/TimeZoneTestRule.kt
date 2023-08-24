ea84d1c63e7899bb1f4d6e4df4f475995cb6cc81
Takao Chiba <chibatching@users.noreply.github.com>
2023-08-24 19:23:00 +0900

    Reset time zone after tests
    Co-authored-by: Jumpei Matsuda <jmatsu.drm+github@gmail.com>

--- core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/TimeZoneTestRule.kt
+++ core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/TimeZoneTestRule.kt
@@ -4,9 +4,15 @@ import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import java.util.TimeZone
 
-class TimeZoneTestRule : TestWatcher() {
+class TimeZoneTestRule() : TestWatcher() {
+    private lateinit var evacuatedTimeZone: TimeZone
+
     override fun starting(description: Description) {
-        super.starting(description)
+        evacuatedTimeZone = TimeZone.getDefault()
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"))
+    }
+    
+    override fun finished(description: Description) {
+        TimeZone.setDefault(evacuatedTimeZone)
     }
 }
