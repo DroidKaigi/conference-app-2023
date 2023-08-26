5ac8ad04db877b821b5a12c575981a3555f88c7f
ked4ma <hades.netherworld@gmail.com>
2023-08-26 19:46:23 +0900

    add doc

--- core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/OnClickHandler.kt
+++ core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/OnClickHandler.kt
@@ -3,6 +3,11 @@ package io.github.droidkaigi.confsched2023.ui
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 
+/**
+ * Invoke "onClick" if current lifecycle isn't operating other navigation.
+ * @param owner a current LifecycleOwner
+ * @param onClick the method that want to invoke
+ */
 fun handleOnClickIfNotNavigating(owner: LifecycleOwner, onClick: () -> Unit) {
     // https://stackoverflow.com/a/76386604/4339442
     val currentState = owner.lifecycle.currentState
