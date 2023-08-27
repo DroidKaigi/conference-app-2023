4735f709849deb177dd2d75382c90df7b4559616
MOTOHARU ASANUMA <mossann0510@outlook.jp>
2023-08-28 00:56:06 +0900

    Change in initialization method
    初期化方法の変更

--- core/data/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/data/remoteconfig/DefaultRemoteConfigApi.kt
+++ core/data/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/data/remoteconfig/DefaultRemoteConfigApi.kt
@@ -4,9 +4,6 @@ import co.touchlab.kermit.Logger
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.remoteconfig.get
 import dev.gitlive.firebase.remoteconfig.remoteConfig
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class DefaultRemoteConfigApi : RemoteConfigApi {
 
@@ -15,13 +12,13 @@ class DefaultRemoteConfigApi : RemoteConfigApi {
     /**
      * If you want to change the interval time to fetch, please change it here
      */
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            firebaseRemoteConfig.settings {
-                minimumFetchIntervalInSeconds = 1 * 60
-            }
-        }
-    }
+//    init {
+//        CoroutineScope(Dispatchers.IO).launch {
+//            firebaseRemoteConfig.settings {
+//                minimumFetchIntervalInSeconds = 1 * 60
+//            }
+//        }
+//    }
 
     override suspend fun getBoolean(key: String): Boolean {
         fetchConfig()
