033f8b036f8196cb2d71a753814c101576a84bf8
fumiya-kume <fumiya.kume@hotmail.com>
2023-08-29 23:14:35 +0900

    Fix oss licenses plugin id

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("droidkaigi.primitive.detekt")
     id("droidkaigi.primitive.android.roborazzi")
     id("droidkaigi.primitive.kover")
-    id("droidkaigi.primitive.osslicenses")
+    id("droidkaigi.primitive.android.osslicenses")
 }
 
 val keystorePropertiesFile = file("keystore.properties")
