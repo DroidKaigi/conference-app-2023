ab19a16bc62c1823ae387e1c7077b3e5c3bb6b09
fumiya-kume <fumiya.kume@hotmail.com>
2023-08-30 01:41:02 +0900

    Avoid unnecessary boxing at TimetableItemDetailScreenTopAppBar

--- feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
+++ feature/sessions/src/main/java/io/github/droidkaigi/confsched2023/sessions/component/TimetableItemDetailScreenTopAppBar.kt
@@ -14,7 +14,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -92,7 +92,7 @@ private fun ResizeableText(
     styles: ImmutableList<TextStyle>,
     overflow: TextOverflow,
 ) {
-    var styleIndex by remember(text) { mutableStateOf(0) }
+    var styleIndex by remember(text) { mutableIntStateOf(0) }
     Text(
         text = text,
         overflow = overflow,
