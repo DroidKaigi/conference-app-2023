e64e76dfc76069d2eb98dd35c6f980d442aa438c
todayama_r <13657682+Corvus400@users.noreply.github.com>
2023-08-29 18:48:15 +0900

    :recycle: Introduce Compose Compiler Metrics. https://speakerdeck.com/moriatsushi/shi-li-karaxue-bujetpack-composenopahuomansugai-shan?slide=33

--- feature/sponsors/build.gradle.kts
+++ feature/sponsors/build.gradle.kts
@@ -1,8 +1,21 @@
+import io.github.droidkaigi.confsched2023.primitive.android
+import io.github.droidkaigi.confsched2023.primitive.kotlinOptions
+
 plugins {
     id("droidkaigi.convention.androidfeature")
 }
 
 android.namespace = "io.github.droidkaigi.confsched2023.feature.sponsors"
+
+android {
+    kotlinOptions {
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-P",
+            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" +
+                project.buildDir.absolutePath + "/compose_metrics"
+        )
+    }
+}
 
 dependencies {
     implementation(projects.core.designsystem)
