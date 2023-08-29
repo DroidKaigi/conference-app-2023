66f32fe3cc817bbf77a96276f6d1a0ea7aa65d1d
todayama_r <13657682+Corvus400@users.noreply.github.com>
2023-08-29 19:53:26 +0900

    :sparkles: Dialog implemented.

--- feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsScreen.kt
+++ feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsScreen.kt
@@ -1,25 +1,40 @@
 package io.github.droidkaigi.confsched2023.stamps
 
 import androidx.annotation.RawRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import io.github.droidkaigi.confsched2023.designsystem.preview.MultiLanguagePreviews
+import io.github.droidkaigi.confsched2023.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched2023.model.Stamp
 import io.github.droidkaigi.confsched2023.stamps.section.StampList
 import io.github.droidkaigi.confsched2023.ui.SnackbarMessageEffect
@@ -99,6 +114,14 @@ private fun StampsScreen(
             bottom = contentPadding.calculateBottomPadding(),
         ),
         content = { innerPadding ->
+            var showDialog by rememberSaveable { mutableStateOf(true) }
+            val onDismiss = { showDialog = false }
+
+            if (showDialog) {
+                StampsScreenDialog(
+                    onDismissRequest = onDismiss,
+                )
+            }
             StampList(
                 stamps = uiState.stamps,
                 onStampsClick = onStampsClick,
@@ -113,4 +136,80 @@ private fun StampsScreen(
             )
         },
     )
+}
+
+@Composable
+fun StampsScreenDialog(
+    onDismissRequest: () -> Unit,
+) {
+    AlertDialog(
+        title = {
+            Text(
+                text = StampsStrings.DialogTitle.asString(),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onSurface,
+                textAlign = TextAlign.Center,
+            )
+        },
+        text = {
+            Column {
+                OrderedListText(
+                    order = 1,
+                    text = StampsStrings.DialogDescription1.asString()
+                )
+                OrderedListText(
+                    order = 2,
+                    text = StampsStrings.DialogDescription2.asString()
+                )
+                OrderedListText(
+                    order = 3,
+                    text = StampsStrings.DialogDescription3.asString()
+                )
+            }
+        },
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            Text(
+                modifier = Modifier
+                    .clickable { onDismissRequest() },
+                text = StampsStrings.DialogConfirmButton.asString(),
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.primary,
+            )
+        }
+    )
+}
+
+@Composable
+fun OrderedListText(
+    order: Int,
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+    ) {
+        Text(
+            text = "$order. ",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
+    }
+}
+
+@MultiLanguagePreviews
+@Composable
+fun StampsScreenDialogPreview() {
+    KaigiTheme {
+        Surface {
+            StampsScreenDialog(
+                onDismissRequest = {},
+            )
+        }
+    }
 }
--- feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsStrings.kt
+++ feature/stamps/src/main/java/io/github/droidkaigi/confsched2023/stamps/StampsStrings.kt
@@ -12,19 +12,39 @@ sealed class StampsStrings : Strings<StampsStrings>(Bindings) {
 
     data object DescriptionNotes : StampsStrings()
 
+    data object DialogTitle : StampsStrings()
+
+    data object DialogConfirmButton: StampsStrings()
+
+    data object DialogDescription1: StampsStrings()
+
+    data object DialogDescription2: StampsStrings()
+
+    data object DialogDescription3: StampsStrings()
+
     private object Bindings : StringsBindings<StampsStrings>(
         Lang.Japanese to { item, _ ->
             when (item) {
                 Title -> "Stamps"
                 Description -> "会場の各部屋に設置されたNFCタグにスマホをかざしてスタンプを集めてみましょう。"
                 DescriptionNotes -> "※ この企画は変更または中止になる可能性があります"
+                DialogTitle -> "アチーブメントに関する\n注意事項"
+                DialogConfirmButton -> "OK"
+                DialogDescription1 -> "アプリを削除してしまうと、データは復元できません。"
+                DialogDescription2 -> "機種やバージョンによって、お手持ちの端末でうまく動作しない場合があるかもしれません。ご理解いただけますと幸いです。"
+                DialogDescription3 -> "この企画は変更される可能性があります。"
             }
         },
-        Lang.English to { item, _ ->
+        Lang.English to { item, bindings ->
             when (item) {
                 Title -> "Stamps"
                 Description -> "Let's collect stamps by holding your smartphone over the NFC tags placed in each room of the venue."
                 DescriptionNotes -> "※ This program is subject to change or cancellation."
+                DialogTitle -> "Notes on Achievements"
+                DialogConfirmButton -> bindings.defaultBinding(item, bindings)
+                DialogDescription1 -> "If you delete the application, data cannot be recovered."
+                DialogDescription2 -> "Depending on the model and version, the application may not work well on your device. We appreciate your understanding."
+                DialogDescription3 -> "This project is subject to change."
             }
         },
         default = Lang.Japanese,
