1f0f7201bad2eb39e297090098af2a61ba3451e1
ryoya ito <ry.itto.github@gmail.com>
2023-08-30 18:49:55 +0900

    try remove plugins, post scripts

--- app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
+++ app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
@@ -5,7 +5,19 @@ brew install openjdk@17
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
 cd "$CI_WORKSPACE"
+echo "arch=arm64" >> local.properties
 ./gradlew :app-ios-shared:assembleSharedReleaseXCFramework
 
 # Enalbe skip plugin validation
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+# workaround for xcode cloud
+brew install mint
+
+mint install cybozu/LicenseList
+mint install SwiftGen/SwiftGen
+
+mint run LicenseList $CI_DERIVED_DATA_PATH $CI_DERIVED_DATA_PATH
+
+DERIVED_SOURCES_DIR=$CI_DERIVED_DATA_PATH
+mint run SwiftGen --config "$CI_WORKSPACE/app-ios/Modules/Sources/Assets/swiftgen.yml"
--- app-ios/Modules/Package.swift
+++ app-ios/Modules/Package.swift
@@ -7,6 +7,7 @@ var package = Package(
     name: "Modules",
     platforms: [
         .iOS(.v16),
+        .macOS(.v12),
     ],
     products: [
         .library(name: "Component", targets: ["Component"]),
@@ -286,3 +287,19 @@ package.targets = package.targets.map { target in
 
     return target
 }
+
+#if canImport(Darwin)
+import Darwin
+
+// Disable plugins on Xcode Cloud CI
+package.targets = package.targets.map { target in
+    if let ciEnvPointer = getenv("CI") {
+        let ciEnv = String(cString: ciEnvPointer)
+        if ciEnv == "TRUE" {
+            target.plugins = []
+        }
+    }
+
+    return target
+}
+#endif
