a98be8296f8bdaf1037c8d4453e840e336e1053e
ryoya ito <ry.itto.github@gmail.com>
2023-08-30 20:57:12 +0900

    comment filter arc

--- app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
+++ app-ios/App/DroidKaigi2023/ci_scripts/ci_post_clone.sh
@@ -5,7 +5,7 @@ brew install openjdk@17
 export PATH="/usr/local/opt/openjdk@17/bin:$PATH"
 
 cd "$CI_WORKSPACE"
-echo "arch=arm64" >> local.properties
+# echo "arch=arm64" >> local.properties
 ./gradlew :app-ios-shared:assembleSharedReleaseXCFramework
 
 # Enalbe skip plugin validation
