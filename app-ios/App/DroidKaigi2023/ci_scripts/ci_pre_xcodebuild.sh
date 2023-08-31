24d864582955a99853a452a0546158ba3e747838
ryoya ito <ry.itto.github@gmail.com>
2023-09-01 02:28:47 +0900

    fix licenselist gen path

--- app-ios/App/DroidKaigi2023/ci_scripts/ci_pre_xcodebuild.sh
+++ app-ios/App/DroidKaigi2023/ci_scripts/ci_pre_xcodebuild.sh
@@ -13,7 +13,8 @@ mint install SwiftGen/SwiftGen
 
 SOURCE_PACKAGES_PATH="$CI_WORKSPACE/app-ios/SourcePackages"
 xcodebuild -resolvePackageDependencies -project $CI_PROJECT_FILE_PATH -scheme DroidKaigi2023 -derivedDataPath $CI_DERIVED_DATA_PATH -clonedSourcePackagesDirPath ./SourcePackages
-mint run LicenseList $CI_DERIVED_DATA_PATH $SOURCE_PACKAGES_PATH
+
+mint run LicenseList "$CI_WORKSPACE/app-ios/Modules/Sources/About/Resources" $SOURCE_PACKAGES_PATH
 
 export DERIVED_SOURCES_DIR="$CI_WORKSPACE/app-ios/Modules/Sources/Assets/"
 mint run SwiftGen --config "$CI_WORKSPACE/app-ios/Modules/Sources/Assets/swiftgen.yml"
