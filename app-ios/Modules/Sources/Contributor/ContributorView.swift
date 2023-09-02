437fdc173fb99fa95c8daa9257a5eca1ee012258
ryoya ito <ry.itto.github@gmail.com>
2023-09-03 03:22:56 +0900

    switch SwiftUIView/ComposeView, refactor di

--- app-ios/Modules/Sources/Contributor/ContributorView.swift
+++ app-ios/Modules/Sources/Contributor/ContributorView.swift
@@ -1,18 +1,25 @@
 import Component
-import KMPContainer
+import Dependencies
 import Model
 import shared
 import SwiftUI
 
 public struct ContributorView: View {
     @State var presentingURL: IdentifiableURL?
-    @ObservedObject var viewModel: ContributorViewModel = .init()
 
     public init() {}
 
     public var body: some View {
+        Group {
+//            ContributorSwiftUIView { url in
+//                presentingURL = .init(string: url)
+//            }
+
+            ContributorComposeView { url in
+                presentingURL = .init(string: url)
+            }
+        }
         ContributorComposeView(
-            viewModel.getRepositoryForCompose(),
             onContributorItemClick: { contributorUrl in
                 presentingURL = IdentifiableURL(string: contributorUrl)
             }
@@ -31,21 +38,58 @@ public struct ContributorView: View {
     ContributorView()
 }
 
-struct ContributorComposeView: UIViewControllerRepresentable {
-    let contributorsRepository: ContributorsRepository
+struct ContributorSwiftUIView: View {
+    @ObservedObject var viewModel: ContributorViewModel = .init()
     let onContributorItemClick: (String) -> Void
 
-    init(_ contributorsRepository: ContributorsRepository, onContributorItemClick: @escaping (String) -> Void) {
-        self.contributorsRepository = contributorsRepository
+    var body: some View {
+        Group {
+            switch viewModel.state.contributors {
+            case .initial, .loading:
+                ProgressView()
+                    .task {
+                        await viewModel.load()
+                    }
+            case .failed:
+                EmptyView()
+            case .loaded(let contributors):
+                ScrollView {
+                    LazyVStack(spacing: 20) {
+                        ForEach(contributors, id: \.id) { contributor in
+                            Button {
+                                if let profileUrl = contributor.profileUrl {
+                                    onContributorItemClick(profileUrl)
+                                }
+                            } label: {
+                                PersonLabel(
+                                    name: contributor.username,
+                                    iconUrlString: contributor.iconUrl
+                                )
+                            }
+                        }
+                    }
+                    .padding(16)
+                }
+            }
+        }
+    }
+}
+
+struct ContributorComposeView: UIViewControllerRepresentable {
+    @Dependency(\.contributorsRepositoryData) var contributorsRepositoryData
+    @Environment(\.colorScheme) var colorScheme
+    let onContributorItemClick: (String) -> Void
+
+    init(onContributorItemClick: @escaping (String) -> Void) {
         self.onContributorItemClick = onContributorItemClick
     }
 
     func makeUIViewController(context: Context) -> UIViewController {
         let vc = DarwinContributorsKt.contributorViewController(
-            contributorsRepository: contributorsRepository,
+            contributorsRepository: contributorsRepositoryData.contributorsRepository,
             onContributorItemClick: onContributorItemClick
         )
-        vc.overrideUserInterfaceStyle = .light
+        vc.overrideUserInterfaceStyle = .init(colorScheme)
         return vc
     }
 
