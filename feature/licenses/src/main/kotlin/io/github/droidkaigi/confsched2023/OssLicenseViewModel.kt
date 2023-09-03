f537b83f0a1a88ac50011b4771b57a138b6c5f8d
fumiya-kume <fumiya.kume@hotmail.com>
2023-09-03 11:31:08 +0900

    Add empty oss license ViewModel

---
+++ feature/licenses/src/main/kotlin/io/github/droidkaigi/confsched2023/OssLicenseViewModel.kt
@@ -0,0 +1,11 @@
+package io.github.droidkaigi.confsched2023
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OssLicenseViewModel @Inject constructor(
+
+) : ViewModel() {
+}
