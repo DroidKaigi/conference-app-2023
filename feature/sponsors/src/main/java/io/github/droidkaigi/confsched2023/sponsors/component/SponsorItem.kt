0fa89283cef32096b0409d0e819bb23453a499dc
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-09-05 21:07:12 +0900

    feat: Reduce memory usage by coil

--- feature/sponsors/src/main/java/io/github/droidkaigi/confsched2023/sponsors/component/SponsorItem.kt
+++ feature/sponsors/src/main/java/io/github/droidkaigi/confsched2023/sponsors/component/SponsorItem.kt
@@ -1,27 +1,22 @@
 package io.github.droidkaigi.confsched2023.sponsors.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Image
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import io.github.droidkaigi.confsched2023.designsystem.preview.MultiLanguagePreviews
 import io.github.droidkaigi.confsched2023.designsystem.preview.MultiThemePreviews
 import io.github.droidkaigi.confsched2023.designsystem.theme.KaigiTheme
 import io.github.droidkaigi.confsched2023.model.Sponsor
 import io.github.droidkaigi.confsched2023.model.fakes
-import io.github.droidkaigi.confsched2023.ui.previewOverride
-import io.github.droidkaigi.confsched2023.ui.rememberAsyncImagePainter
 
 @Composable
 fun SponsorItem(
@@ -29,10 +24,8 @@ fun SponsorItem(
     modifier: Modifier = Modifier,
     onSponsorClick: (url: String) -> Unit,
 ) {
-    Image(
-        painter = previewOverride(previewPainter = { rememberVectorPainter(image = Icons.Default.Image) }) {
-            rememberAsyncImagePainter(sponsor.logo)
-        },
+    AsyncImage(
+        model = sponsor.logo,
         contentDescription = sponsor.name,
         modifier = modifier
             .padding(
