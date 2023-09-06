7e2ef4f846651e47e80358ab7c7986fb18ed592c
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-09-07 02:09:01 +0900

    apply theme to contributors screen with workaround

--- core/designsystem/src/iosMain/kotlin/io/github/droidkaigi/confsched2023/designsystem/theme/FontResource.kt
+++ core/designsystem/src/iosMain/kotlin/io/github/droidkaigi/confsched2023/designsystem/theme/FontResource.kt
@@ -1,15 +1,8 @@
 package io.github.droidkaigi.confsched2023.designsystem.theme
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.platform.Font
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.resource
 
 val fontMap: HashMap<String, FontFamily?> = HashMap()
 
@@ -17,21 +10,23 @@ val fontMap: HashMap<String, FontFamily?> = HashMap()
 @ExperimentalResourceApi
 @Composable
 actual fun fontFamilyResource(fontResource: FontResource): FontFamily? {
-    var fontFamily: FontFamily? by
-        remember(fontResource.resName) { mutableStateOf(fontMap[fontResource.resName]) }
-    if (fontFamily == null) {
-        LaunchedEffect(fontResource.resName) {
-            fontFamily = try {
-                val font = Font(
-                    fontResource.resName,
-                    resource("font/${fontResource.resName}").readBytes(),
-                )
-                fontMap[fontResource.resName] = FontFamily(font)
-                fontMap[fontResource.resName]
-            } catch (e: Exception) {
-                throw IllegalArgumentException(e)
-            }
-        }
-    }
-    return fontFamily
+    // FIXME: Load the custom font in resources directory
+    return FontFamily.Default
+//    var fontFamily: FontFamily? by
+//        remember(fontResource.resName) { mutableStateOf(fontMap[fontResource.resName]) }
+//    if (fontFamily == null) {
+//        LaunchedEffect(fontResource.resName) {
+//            fontFamily = try {
+//                val font = Font(
+//                    fontResource.resName,
+//                    resource("font/${fontResource.resName}").readBytes(),
+//                )
+//                fontMap[fontResource.resName] = FontFamily(font)
+//                fontMap[fontResource.resName]
+//            } catch (e: Exception) {
+//                throw IllegalArgumentException(e)
+//            }
+//        }
+//    }
+//    return fontFamily
 }
