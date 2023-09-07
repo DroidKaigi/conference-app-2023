4275f50f7dee4899b19d75d497e4fac34090e481
takahirom <takam.dev@gmail.com>
2023-09-07 20:25:30 +0900

    Fix crashlytics

--- build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/AndroidCrashlyticsPlugin.kt
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/AndroidCrashlyticsPlugin.kt
@@ -2,6 +2,7 @@ package io.github.droidkaigi.confsched2023.primitive
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 @Suppress("unused")
 class AndroidCrashlyticsPlugin : Plugin<Project> {
@@ -9,6 +10,9 @@ class AndroidCrashlyticsPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("com.google.firebase.crashlytics")
+            }
+            dependencies {
+                implementation(libs.library("firebaseCrashlytics"))
             }
         }
     }
