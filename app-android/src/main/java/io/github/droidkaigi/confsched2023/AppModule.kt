20132770b7658685935938cf4c01589186ea2434
Taichi Sato <info@syarihu.net>
2023-09-07 22:13:35 +0900

    Add AppModule to app-android module to provide BuildConfig from app

---
+++ app-android/src/main/java/io/github/droidkaigi/confsched2023/AppModule.kt
@@ -0,0 +1,20 @@
+package io.github.droidkaigi.confsched2023
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.droidkaigi.confsched2023.model.BuildConfigProvider
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideBuildConfigProvider(): BuildConfigProvider = AppBuildConfigProvider()
+}
+
+class AppBuildConfigProvider(
+    override val versionName: String = BuildConfig.VERSION_NAME,
+) : BuildConfigProvider
