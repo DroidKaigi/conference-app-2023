46fc71704a3a9fcbc263ef803a8215789ab221d1
todayama_r <13657682+Corvus400@users.noreply.github.com>
2023-09-10 12:05:31 +0900

    :wrench: Variable all structs to public.

--- app-ios/Modules/Sources/Theme/Spacing.swift
+++ app-ios/Modules/Sources/Theme/Spacing.swift
@@ -1,21 +1,21 @@
 import SwiftUI
 
 public struct Spacing {
-    var xxxs: CGFloat = SpacingTokens.xxxs
-    var xxs: CGFloat = SpacingTokens.xxs
-    var xs: CGFloat = SpacingTokens.xs
-    var s: CGFloat = SpacingTokens.s
-    var m: CGFloat = SpacingTokens.m
-    var l: CGFloat = SpacingTokens.l
-    var xl: CGFloat = SpacingTokens.xl
+    public var xxxs: CGFloat = SpacingTokens.xxxs
+    public var xxs: CGFloat = SpacingTokens.xxs
+    public var xs: CGFloat = SpacingTokens.xs
+    public var s: CGFloat = SpacingTokens.s
+    public var m: CGFloat = SpacingTokens.m
+    public var l: CGFloat = SpacingTokens.l
+    public var xl: CGFloat = SpacingTokens.xl
 }
 
 public struct SpacingTokens {
-    static let xxxs: CGFloat = 2
-    static let xxs: CGFloat = 4
-    static let xs: CGFloat = 8
-    static let s: CGFloat = 12
-    static let m: CGFloat = 16
-    static let l: CGFloat = 20
-    static let xl: CGFloat = 24
+    public static let xxxs: CGFloat = 2
+    public static let xxs: CGFloat = 4
+    public static let xs: CGFloat = 8
+    public static let s: CGFloat = 12
+    public static let m: CGFloat = 16
+    public static let l: CGFloat = 20
+    public static let xl: CGFloat = 24
 }
