eb9e589334af258b1afb2ad966f53911d797c344
todayama_r <13657682+Corvus400@users.noreply.github.com>
2023-09-10 11:43:27 +0900

    :wrench: Revised to type-safe form.

--- app-ios/Modules/Sources/Theme/Typography.swift
+++ app-ios/Modules/Sources/Theme/Typography.swift
@@ -19,26 +19,25 @@ struct Typography {
 }
 
 struct TextStyle {
-    var font: Font
+    var font: SwiftUI.Font
     var lineHeight: CGFloat
     var letterSpacing: CGFloat?
 }
 
 struct TypographyTokens {
-    static let DisplayLarge = TextStyle(font: FontAssets.Montserrat.regular.font(size: 57), lineHeight: 64)
-
-    static let DisplayMedium = TextStyle(fontSize: 45, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 45), lineHeight: 52)
-    static let DisplaySmall = TextStyle(fontSize: 36, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 36), lineHeight: 44)
-    static let HeadlineLarge = TextStyle(fontSize: 32, fontFamily: "Montserrat-Medium", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-Medium", size: 32), lineHeight: 40)
-    static let HeadlineMedium = TextStyle(fontSize: 28, fontFamily: "Montserrat-Medium", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-Medium", size: 28), lineHeight: 36)
-    static let HeadlineSmall = TextStyle(fontSize: 24, fontFamily: "Montserrat-Medium", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-Medium", size: 24), lineHeight: 32)
-    static let TitleLarge = TextStyle(fontSize: 22, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 22), lineHeight: 28)
-    static let TitleMedium = TextStyle(fontSize: 16, fontFamily: "Montserrat-SemiBold", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-SemiBold", size: 16), lineHeight: 24, letterSpacing: 0.15)
-    static let TitleSmall = TextStyle(fontSize: 14, fontFamily: "Montserrat-SemiBold", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-SemiBold", size: 14), lineHeight: 20, letterSpacing: 0.1)
-    static let LabelLarge = TextStyle(fontSize: 14, fontFamily: "Montserrat-SemiBold", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-SemiBold", size: 14), lineHeight: 20, letterSpacing: 0.1)
-    static let LabelMedium = TextStyle(fontSize: 12, fontFamily: "Montserrat-SemiBold", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-SemiBold", size: 12), lineHeight: 16, letterSpacing: 0.5)
-    static let LabelSmall = TextStyle(fontSize: 11, fontFamily: "Montserrat-SemiBold", fontWeight: .medium, fontStyle: SwiftUI.Font.custom("Montserrat-SemiBold", size: 11), lineHeight: 16, letterSpacing: 0.5)
-    static let BodyLarge = TextStyle(fontSize: 16, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 16), lineHeight: 24, letterSpacing: 0.15)
-    static let BodyMedium = TextStyle(fontSize: 14, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 14), lineHeight: 20, letterSpacing: 0.25)
-    static let BodySmall = TextStyle(fontSize: 12, fontFamily: "Montserrat-Regular", fontWeight: .regular, fontStyle: SwiftUI.Font.custom("Montserrat-Regular", size: 12), lineHeight: 16, letterSpacing: 0.4)
+    static let DisplayLarge = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 57), lineHeight: 64)
+    static let DisplayMedium = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 45), lineHeight: 52)
+    static let DisplaySmall = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 36), lineHeight: 44)
+    static let HeadlineLarge = TextStyle(font: SwiftUI.Font.custom("Montserrat-Medium", size: 32), lineHeight: 40)
+    static let HeadlineMedium = TextStyle(font: SwiftUI.Font.custom("Montserrat-Medium", size: 28), lineHeight: 36)
+    static let HeadlineSmall = TextStyle(font: SwiftUI.Font.custom("Montserrat-Medium", size: 24), lineHeight: 32)
+    static let TitleLarge = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 22), lineHeight: 28)
+    static let TitleMedium = TextStyle(font: SwiftUI.Font.custom("Montserrat-SemiBold", size: 16), lineHeight: 24, letterSpacing: 0.15)
+    static let TitleSmall = TextStyle(font: SwiftUI.Font.custom("Montserrat-SemiBold", size: 14), lineHeight: 20, letterSpacing: 0.1)
+    static let LabelLarge = TextStyle(font: SwiftUI.Font.custom("Montserrat-SemiBold", size: 14), lineHeight: 20, letterSpacing: 0.1)
+    static let LabelMedium = TextStyle(font: SwiftUI.Font.custom("Montserrat-SemiBold", size: 12), lineHeight: 16, letterSpacing: 0.5)
+    static let LabelSmall = TextStyle(font: SwiftUI.Font.custom("Montserrat-SemiBold", size: 11), lineHeight: 16, letterSpacing: 0.5)
+    static let BodyLarge = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 16), lineHeight: 24, letterSpacing: 0.15)
+    static let BodyMedium = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 14), lineHeight: 20, letterSpacing: 0.25)
+    static let BodySmall = TextStyle(font: SwiftUI.Font.custom("Montserrat-Regular", size: 12), lineHeight: 16, letterSpacing: 0.4)
 }
