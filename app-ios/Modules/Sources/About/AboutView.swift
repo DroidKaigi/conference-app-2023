ddd1f9d3fc78c6118c76332aa8a6dacd8ec44d49
woxtu <woxtup@gmail.com>
2023-09-11 22:26:16 +0900

    Update the top image on About

--- app-ios/Modules/Sources/About/AboutView.swift
+++ app-ios/Modules/Sources/About/AboutView.swift
@@ -35,6 +35,8 @@ public struct AboutView<ContributorView: View, StaffView: View, SponsorView: Vie
             ScrollView {
                 VStack(spacing: 0) {
                     Assets.Images.aboutKeyVisual.swiftUIImage
+                        .resizable()
+                        .frame(maxWidth: .infinity)
                     Spacer().frame(height: 16)
                     Text(L10n.About.description)
                         .font(Font.custom(FontAssets.Montserrat.medium, size: 16))
