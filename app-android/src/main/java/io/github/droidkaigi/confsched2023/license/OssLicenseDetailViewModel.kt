0294a8891f8876acefdf14136c9d43e7ed36e5eb
fumiya-kume <fumiya.kume@hotmail.com>
2023-09-12 21:39:59 +0900

    Add OssDetailLicense screen

---
+++ app-android/src/main/java/io/github/droidkaigi/confsched2023/license/OssLicenseDetailViewModel.kt
@@ -0,0 +1,59 @@
+package io.github.droidkaigi.confsched2023.license
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.droidkaigi.confsched2023.ui.buildUiState
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class OssLicenseDetailViewModel @Inject constructor(
+    private val ossLicenseRepository: OssLicenseRepository,
+    savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    private val licenseName = savedStateHandle.getStateFlow<String>(
+        ossLicenseDetailScreenRouteNameArgument,
+        "",
+    )
+
+    private val licenseStateFlow: StateFlow<PersistentList<License>> =
+        ossLicenseRepository.licenseMetaData()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = persistentListOf(),
+            )
+
+    private val licenseDetailStateFlow: StateFlow<List<String>> =
+        ossLicenseRepository.licenseDetailData()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = emptyList(),
+            )
+
+    internal val uiState: StateFlow<OssLicenseDetailScreenUiState> =
+        buildUiState(licenseStateFlow, licenseDetailStateFlow) { metadata, detail ->
+            val license = metadata.firstOrNull { it.name.replace(' ', '-') == licenseName.value }
+                ?.let { license ->
+                    val detail = kotlin.runCatching {
+                        val start = license.offset
+                        val end = license.offset + license.length
+                        detail.subList(
+                            fromIndex = start,
+                            toIndex = end,
+                        ).joinToString("\n")
+                    }.getOrElse { "" }
+                    license.copy(detail = detail)
+                }
+
+            OssLicenseDetailScreenUiState(license)
+        }
+}
