cc036f078be70aa4daa7fcd0583f445530129a58
woxtu <woxtup@gmail.com>
2023-09-12 22:26:47 +0900

    Localize Bookmarks

--- app-ios/Modules/Sources/Timetable/Bookmark/BookmarkView.swift
+++ app-ios/Modules/Sources/Timetable/Bookmark/BookmarkView.swift
@@ -28,7 +28,7 @@ struct BookmarkView<SessionView: View>: View {
                 VStack(alignment: .leading, spacing: 0) {
                     HStack {
                         FilterLabel(
-                            title: "全て",
+                            title: L10n.Bookmark.allDay,
                             isSelection: false,
                             isSelected: viewModel.state.selectedDay == nil,
                             onSelect: {
@@ -68,7 +68,7 @@ struct BookmarkView<SessionView: View>: View {
             }
         }
         .background(AssetColors.Surface.surface.swiftUIColor)
-        .navigationTitle("Bookmark")
+        .navigationTitle(L10n.Bookmark.title)
     }
 }
 
@@ -82,12 +82,12 @@ private struct BookmarkEmptyView: View {
                 .background(AssetColors.Secondary.secondaryContainer.swiftUIColor)
                 .clipShape(RoundedRectangle(cornerRadius: 24))
             Spacer().frame(height: 24)
-            Text("登録されたセッションがありません")
+            Text(L10n.Bookmark.bookmarksNotFound)
                 .font(Font.custom(FontAssets.Montserrat.medium, size: 22))
                 .multilineTextAlignment(.center)
                 .foregroundStyle(AssetColors.Surface.onSurface.swiftUIColor)
             Spacer().frame(height: 8)
-            Text("気になるセッションをブックマークに追加して集めてみましょう！")
+            Text(L10n.Bookmark.bookmarksNotFoundNote)
                 .font(Font.custom(FontAssets.Montserrat.medium, size: 14))
                 .multilineTextAlignment(.center)
                 .foregroundStyle(AssetColors.Surface.onSurfaceVariant.swiftUIColor)
