518615bbf0ad7002b159e908500d69c1f798a466
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-09-16 17:24:54 +0900

    fix format

--- core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/compositionlocal/LocalClockProvider.kt
+++ core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/compositionlocal/LocalClockProvider.kt
@@ -3,7 +3,7 @@ package io.github.droidkaigi.confsched2023.ui.compositionlocal
 import androidx.compose.runtime.staticCompositionLocalOf
 import kotlinx.datetime.Clock
 
-@Suppress("CompositionLocalAllowList")
+@Suppress("CompositionLocalAllowlist")
 val LocalClockProvider = staticCompositionLocalOf<Clock> {
     Clock.System
 }
