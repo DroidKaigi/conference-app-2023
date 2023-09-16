9c90ff80177dedf71cfb5af4ba8404411ee9df02
kenken <43767445+tkhs0604@users.noreply.github.com>
2023-09-16 17:17:40 +0900

    replace with staticCompositionLocalOf

--- core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/compositionlocal/LocalClockProvider.kt
+++ core/ui/src/androidMain/kotlin/io/github/droidkaigi/confsched2023/ui/compositionlocal/LocalClockProvider.kt
@@ -1,9 +1,9 @@
 package io.github.droidkaigi.confsched2023.ui.compositionlocal
 
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import kotlinx.datetime.Clock
 
 @Suppress("CompositionLocalAllowList")
-val LocalClockProvider = compositionLocalOf<Clock> {
+val LocalClockProvider = staticCompositionLocalOf<Clock> {
     Clock.System
 }
