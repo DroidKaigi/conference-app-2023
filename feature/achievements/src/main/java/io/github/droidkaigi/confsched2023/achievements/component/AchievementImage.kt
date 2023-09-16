0f0fb8799fe1203cac86f5619afedf84832c03ed
gy6543721 <gy6543721@yahoo.com.tw>
2023-09-16 13:26:37 +0900

    Delete inner padding

--- feature/achievements/src/main/java/io/github/droidkaigi/confsched2023/achievements/component/AchievementImage.kt
+++ feature/achievements/src/main/java/io/github/droidkaigi/confsched2023/achievements/component/AchievementImage.kt
@@ -2,11 +2,9 @@ package io.github.droidkaigi.confsched2023.achievements.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched2023.model.AchievementAnimation
 
 @Composable
@@ -17,8 +15,6 @@ fun AchievementImage(
     Image(
         painter = painterResource(id = achievementAnimation.getDrawableResId()),
         contentDescription = achievementAnimation.contentDescription,
-        modifier = modifier
-            .fillMaxSize()
-            .padding(horizontal = 21.dp),
+        modifier = modifier.fillMaxSize(),
     )
 }
