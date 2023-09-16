f96c2e36cc8adc663d173d1da507d8e4869cb393
gy6543721 <gy6543721@yahoo.com.tw>
2023-09-16 15:27:22 +0900

    AchievementImage size modification

--- feature/achievements/src/main/java/io/github/droidkaigi/confsched2023/achievements/component/AchievementImage.kt
+++ feature/achievements/src/main/java/io/github/droidkaigi/confsched2023/achievements/component/AchievementImage.kt
@@ -1,10 +1,12 @@
 package io.github.droidkaigi.confsched2023.achievements.component
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import io.github.droidkaigi.confsched2023.model.AchievementAnimation
 
 @Composable
@@ -12,9 +14,12 @@ fun AchievementImage(
     achievementAnimation: AchievementAnimation,
     modifier: Modifier = Modifier,
 ) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp.dp
+
     Image(
         painter = painterResource(id = achievementAnimation.getDrawableResId()),
         contentDescription = achievementAnimation.contentDescription,
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier.width(screenWidth / 3),
     )
 }
