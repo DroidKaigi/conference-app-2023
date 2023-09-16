6b6c2594136da0bf6222b5bf5a3cb7a28609bf2c
rey <ku.k669500@gmail.com>
2023-09-16 16:22:35 +0900

    Migrate Dagger hilt from kapt to ksp

--- build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KmpAndroidHiltPlugin.kt
+++ build-logic/src/main/kotlin/io/github/droidkaigi/confsched2023/primitive/KmpAndroidHiltPlugin.kt
@@ -3,22 +3,21 @@ package io.github.droidkaigi.confsched2023.primitive
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
-import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.get
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 @Suppress("unused")
 class KmpAndroidHiltPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                apply("kotlin-kapt")
+                apply("com.google.devtools.ksp")
                 apply("dagger.hilt.android.plugin")
             }
             kotlin {
                 sourceSets.getByName("androidMain") {
-                    val kaptConfiguration = configurations["kapt"]
-                    kaptConfiguration.dependencies.add(
+                    val kspConfiguration = configurations["ksp"]
+                    kspConfiguration.dependencies.add(
                         libs.library("daggerHiltAndroidCompiler").let {
                             DefaultExternalModuleDependency(
                                 it.module.group,
@@ -34,8 +33,11 @@ class KmpAndroidHiltPlugin : Plugin<Project> {
                     }
                 }
             }
-            extensions.configure<KaptExtension> {
-                correctErrorTypes = true
+
+            dependencies {
+                (listOf("CommonMainMetadata", "Android")).forEach {
+                    add("ksp$it", libs.library("daggerHiltAndroid"))
+                }
             }
         }
     }
