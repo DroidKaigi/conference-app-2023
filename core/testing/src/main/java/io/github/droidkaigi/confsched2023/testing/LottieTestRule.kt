fcfd0059c90069d9c87b75e77636bc412659f540
Aniokrait <itititcreclickhub@gmail.com>
2023-09-30 09:40:08 +0900

    change reset logic

--- core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/LottieTestRule.kt
+++ core/testing/src/main/java/io/github/droidkaigi/confsched2023/testing/LottieTestRule.kt
@@ -4,16 +4,15 @@ import com.airbnb.lottie.LottieTask
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class LottieTestRule : TestWatcher() {
-    private lateinit var evacuatedExecutor: Executor
 
     override fun starting(description: Description) {
-        evacuatedExecutor = LottieTask.EXECUTOR
         LottieTask.EXECUTOR = Executor(Runnable::run)
     }
 
     override fun finished(description: Description) {
-        LottieTask.EXECUTOR = evacuatedExecutor
+        LottieTask.EXECUTOR = Executors.newCachedThreadPool()
     }
 }
